327e1004fd234bfb57913ebe47cb051ea02107fe
gsparvej <parvejmd4469@gmail.com>
2025-06-15 12:20:04 +0600

    add

--- Employee/src/java/dao/EmployeeDao.java
+++ Employee/src/java/dao/EmployeeDao.java
@@ -2,9 +2,12 @@ package dao;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import model.Employee;
 import util.DbUtil;
 
@@ -17,15 +20,56 @@ public class EmployeeDao {
     public static List<Employee> getAllEmployees() {
 
         List<Employee> employees = new ArrayList<>();
+        
         sql = "select * from employees";
-        ps = DbUtil.getCon().
        
-    
-    
-    
-    
-    
-    
+        try {
+            ps = DbUtil.getCon().prepareStatement(sql);
+            rs=ps.executeQuery();
+            
+            while(rs.next()){
+            
+                Employee e=new Employee(rs.getInt("empId"),
+                        rs.getString("name"),
+                        rs.getString("email"),
+                        rs.getFloat("salary")
+                
+                );
+            employees.add(e);
+            
+            }
+            rs.close();
+            ps.close();
+            DbUtil.getCon().close();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(EmployeeDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       
         return employees;
     }
+    
+    public static int saveEmployee(Employee e){
+    
+        int status= 0;
+        sql="insert into employees (name,email,salary) values(?,?,?)";
+        try {
+            ps=DbUtil.getCon().prepareStatement(sql);
+            
+            ps.setString(1, e.getName());
+            ps.setString(2, e.getEmail());
+            ps.setFloat(3, e.getSalary());
+            
+            status=ps.executeUpdate();
+            
+            ps.close();
+            DbUtil.getCon().close();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(EmployeeDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      
+    return status;
+    }
+            
 }
--- Employee/src/java/util/DbUtil.java
+++ Employee/src/java/util/DbUtil.java
@@ -2,13 +2,38 @@
 
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 
 public class DbUtil {
 
-    public static Object getCon() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-    
+     private static Connection con = null;
+    private static String url = "jdbc:mysql://localhost:3306/jsp";
+    private static String user = "root";
+    private static String password = "1234";
+    private static String driver = "com.mysql.cj.jdbc.Driver";
+
+   public static Connection getCon() {
+   
+        try {
+            Class.forName(driver);
+            try {
+                con=DriverManager.getConnection(url, user, password);
+            } catch (SQLException ex) {
+                Logger.getLogger(DbUtil.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DbUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+   
+   
+   return con;
+   }
     
 }

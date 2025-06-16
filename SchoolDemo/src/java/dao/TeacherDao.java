02d2e3a45f00799e63774e9159191f071c211a72
gsparvej <parvejmd4469@gmail.com>
2025-06-17 01:15:49 +0600

    add

---
+++ SchoolDemo/src/java/dao/TeacherDao.java
@@ -0,0 +1,132 @@
+
+package dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Teacher;
+import util.SchUtil;
+
+
+public class TeacherDao {
+    
+    static PreparedStatement ps;
+    static ResultSet rs;
+    static String sql;
+    
+    public static int saveTeacher(Teacher t){
+    int status=0;
+    sql="insert into teachers(tName,tDepartment,tSalary,tAddress,tContact) values(?,?,?,?,?)";
+        try {
+            ps=SchUtil.getCon().prepareStatement(sql);
+            ps.setString(1, t.gettName());
+            ps.setString(2, t.gettDepartment());
+            ps.setFloat(3, t.gettSalary());
+            ps.setString(4, t.gettAddress());
+            ps.setString(5, t.gettContact());
+            
+           status= ps.executeUpdate();
+            
+            ps.close();
+            SchUtil.getCon().close();
+        } catch (SQLException ex) {
+            Logger.getLogger(TeacherDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    return status;
+    }
+    
+    
+    public static List<Teacher> getAllTeacher(){
+    
+        List<Teacher> tea=new ArrayList<>();
+        
+        sql="select * from teachers";
+        try {
+            ps=SchUtil.getCon().prepareStatement(sql);
+            rs=ps.executeQuery();
+            
+            while(rs.next()){
+            
+                Teacher t=new Teacher(rs.getInt("tId"),
+                        rs.getString("tName"),
+                        rs.getString("tDepartment"),
+                        rs.getFloat("tSalary"),
+                        rs.getString("tAddress"),
+                        rs.getString("tContact")
+                
+                );
+                tea.add(t);
+            
+            }
+            
+            rs.close();
+            ps.close();
+            SchUtil.getCon().close();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(TeacherDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    return tea;
+    }
+    
+    
+    public static void deleteTeacher(int tId){
+    sql="delete from teachers where tId=?";
+    
+        try {
+            ps=SchUtil.getCon().prepareStatement(sql);
+            ps.setInt(1, tId);
+            
+            ps.executeUpdate();
+            
+            ps.close();
+            SchUtil.getCon().close();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(TeacherDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    
+    }
+    
+    public static Teacher getById(int tId){
+    
+        Teacher t=null;
+        
+        sql="select * from teachers where tId=?";
+        
+        try {
+            ps=SchUtil.getCon().prepareStatement(sql);
+            ps.setInt(1, tId);
+           rs=ps.executeQuery();
+           
+           while(rs.next()){
+                 t=new Teacher(rs.getInt("tId"),
+                        rs.getString("tName"),
+                        rs.getString("tDepartment"),
+                        rs.getFloat("tSalary"),
+                        rs.getString("tAddress"),
+                        rs.getString("tContact")
+                
+                );
+           }
+           rs.close();
+           ps.close();
+           SchUtil.getCon().close();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(TeacherDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    
+    
+    return t;
+    
+    }
+}
--- SchoolDemo/src/java/model/Teacher.java
+++ SchoolDemo/src/java/model/Teacher.java
@@ -10,4 +10,80 @@ package model;
  */
 public class Teacher {
     
+    private int tId;
+    private String tName;
+    private String tDepartment;
+    private float tSalary;
+    private String tAddress;
+    private String tContact;
+
+    public Teacher() {
+    }
+
+    public Teacher(int tId, String tName, String tDepartment, float tSalary, String tAddress, String tContact) {
+        this.tId = tId;
+        this.tName = tName;
+        this.tDepartment = tDepartment;
+        this.tSalary = tSalary;
+        this.tAddress = tAddress;
+        this.tContact = tContact;
+    }
+
+    public Teacher(String tName, String tDepartment, float tSalary, String tAddress, String tContact) {
+        this.tName = tName;
+        this.tDepartment = tDepartment;
+        this.tSalary = tSalary;
+        this.tAddress = tAddress;
+        this.tContact = tContact;
+    }
+
+    public int gettId() {
+        return tId;
+    }
+
+    public void settId(int tId) {
+        this.tId = tId;
+    }
+
+    public String gettName() {
+        return tName;
+    }
+
+    public void settName(String tName) {
+        this.tName = tName;
+    }
+
+    public String gettDepartment() {
+        return tDepartment;
+    }
+
+    public void settDepartment(String tDepartment) {
+        this.tDepartment = tDepartment;
+    }
+
+    public float gettSalary() {
+        return tSalary;
+    }
+
+    public void settSalary(float tSalary) {
+        this.tSalary = tSalary;
+    }
+
+    public String gettAddress() {
+        return tAddress;
+    }
+
+    public void settAddress(String tAddress) {
+        this.tAddress = tAddress;
+    }
+
+    public String gettContact() {
+        return tContact;
+    }
+
+    public void settContact(String tContact) {
+        this.tContact = tContact;
+    }
+    
+    
 }
